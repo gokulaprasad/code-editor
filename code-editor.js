5271e168af77da518e5e4c22b8fc9e844626faf0
Gokulaprasad <106874580+gokulaprasad@users.noreply.github.com>
2023-11-27 20:16:28 +0530

    Add files via upload

---
+++ code-editor.js
@@ -0,0 +1,17 @@
+function theme_mode()
+{
+    var colour = document.body;
+    colour.classList.toggle('dark-mode');
+}
+
+function run()
+{
+    let htmlcode = document.getElementById("html-code");
+    let csscode = document.getElementById("css-code");
+    let jscode = document.getElementById("js-code");
+    let output = document.getElementById("output");
+
+    output.contentDocument.body.innerHTML = htmlcode.value + "<style>" + csscode.value+ "</style>";
+
+    output.contentWindow.eval(jscode.value);
+}
